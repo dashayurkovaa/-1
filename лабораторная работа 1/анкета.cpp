af229f0132ed001c6323077c54c7fdde5dc23a42
dashayurkovaa <144125536+dashayurkovaa@users.noreply.github.com>
2023-10-03 15:06:56 +0300

    анкета.cpp

--- лабораторная работа 1/анкета.cpp
+++ лабораторная работа 1/анкета.cpp
@@ -1,16 +1,18 @@
 #include <iostream>
-
-int account()
+#include <windows.h>
+int main()
 {
-    std::cout << "Yurkova \n";
-    std::cout << "Daria \n";
-    std::cout << "Vladimirovna \n";
-    std::cout << "Woman \n";
-    std::cout << "2006-09-17 \n";
-    std::cout << "Butova 240,Dnepr city \n";
-    std::cout << "+380976491163 \n";
-    std::cout << "MMF, DNU named after Oles Gonchar \n";
-    std::cout << "MA-23-1 \n";
-    std::cout << "I love to draw and sing \n";
-    return 0;
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+    std::cout << "       Прізвище : Yurkova \n";
+    std::cout << "           Ім`я : Daria \n";
+    std::cout << "    По батькові : Vladimirovna \n";
+    std::cout << "          Стать : Woman \n";
+    std::cout << "Дата народження : 2006-09-17 \n";    
+    std::cout << "         Адреса : Butova 240,Dnepr city \n";
+    std::cout << " Номер телефону : +380976491163 \n";
+    std::cout << " Місце навчання : MMF, DNU named after Oles Gonchar \n";
+    std::cout << "          Група : MA-23-1 \n";
+    std::cout << "           Хобі : I love to draw and sing \n";
+    system("pause");
 }
